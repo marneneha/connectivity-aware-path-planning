261b0f15dd492a9f3db44983cf9b1553ec11bae0
nehamarne27 <nehamarne27@gmail.com>
2023-09-13 21:12:07 -0400

    added set objective properly yet to add constraints

--- utilities/gurobi.py
+++ utilities/gurobi.py
@@ -14,29 +14,32 @@ u = m.addMVar(shape=(12, ), vtype=GRB.CONTINUOUS, name="u")
 x1 = m.addVar(name="x1")
 obj = m.addVar(name="obj")
 u_diff = m.addVar(name="u_diff")
-u_diff = u-np.array([1,0,1,0,1,0,1,0,1,0,1,0])
+# u_diff = u-np.array([1,0,1,0,1,0,1,0,1,0,1,0])
 u_ref = np.array([1,0,1,0,1,0,1,0,1,0,1,0])
-c0 = 0
-
+obj =0
+for i in range(12):
+    obj = obj+((u[i]-u_ref[i])*(u[i]-u_ref[i]))
+# obj = ( for i in range(12) )
 # GRBQuadExpr operator-(GRBQuadExpr u_ref,GRBQuadExpr u)
 # obj = gp.norm(u-[1,0,1,0,1,0,1,0,1,0,1,0])
 # m.addGenConstrNorm(obj, u_diff, 2.0, "normconstr")
-obj = np.linalg.norm(u-u_ref)**2
+# obj = np.linalg.norm(u-u_ref)**2
 # print(obj)
 m.setObjective(obj, GRB.MINIMIZE)
 # # dynamics constraints
 # m.addConstr(x1 ==[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]+u*0.1)
-# # safety constraints
-# # m.addConstr(np.linalg.norm(x-x)>1)
+# # # safety constraints
+for i,j in range(12):
+    m.addGenConstrNorm(np.linalg.norm(x1[i]-x1[j])>1)
 # # connectivity constraints
 # # m.addConstr(np.linalg.norm(x-x)<Rc)
 # # m.addConstr(np.linalg.norm(x-x)>Rdc)
 
 
-# m.optimize()
-# print(m.SolCount)
-# for v in m.getVars():
-#     print("i am here")
-#     print('%s %g' % (v.VarName, v.X))
+m.optimize()
+print(m.SolCount)
+for v in m.getVars():
+    print("i am here")
+    print('%s %g' % (v.VarName, v.X))
 
-# print('Obj: %g' % obj.getValue())
+print('Obj: %g' % obj.getValue())
